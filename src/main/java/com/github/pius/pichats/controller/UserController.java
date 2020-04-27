de18c0189696354640948d182c5863eeb8c70ed3
PiusAremieye <ebi44ril@gmail.com>
2020-04-27 08:36:06 +0100

    moved methods in bio and profile controllers to users controller

--- src/main/java/com/github/pius/pichats/controller/UserController.java
+++ src/main/java/com/github/pius/pichats/controller/UserController.java
@@ -1,21 +1,19 @@
 package com.github.pius.pichats.controller;
 
 import com.github.pius.pichats.apiresponse.ApiResponse;
-import com.github.pius.pichats.dto.LoginRequestDTO;
-import com.github.pius.pichats.dto.LoginResponseDTO;
-import com.github.pius.pichats.dto.SearchUsernameDto;
-import com.github.pius.pichats.dto.SignupRequestDTO;
+import com.github.pius.pichats.dto.*;
+import com.github.pius.pichats.model.Bio;
+import com.github.pius.pichats.model.ProfilePic;
 import com.github.pius.pichats.model.User;
 import com.github.pius.pichats.service.AuthService;
+import com.github.pius.pichats.service.BioService;
+import com.github.pius.pichats.service.ProfileService;
 import com.github.pius.pichats.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -24,14 +22,55 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
   private UserService userService;
+  private BioService bioService;
+  private ProfileService profileService;
+  private ModelMapper modelMapper;
 
   @Autowired
-  public UserController(UserService userService) {
+  public UserController(UserService userService, BioService bioService, ProfileService profileService, ModelMapper modelMapper) {
     this.userService = userService;
+    this.bioService = bioService;
+    this.profileService = profileService;
+    this.modelMapper = modelMapper;
+  }
+
+  @PostMapping("/bio")
+  public ResponseEntity<ApiResponse<Bio>> addBio(@Valid @RequestBody BioDTO bio, HttpServletRequest request){
+    Bio newBio = bioService.addOrUpdate(modelMapper.map(bio, Bio.class), request);
+    ApiResponse<Bio> response = new ApiResponse<>(HttpStatus.CREATED);
+    response.setData(newBio);
+    response.setMessage("A user has updated is bio");
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/bio")
+  public ResponseEntity<ApiResponse<Bio>> getBio(HttpServletRequest request){
+    Bio foundBio = bioService.find(request);
+    ApiResponse<Bio> response = new ApiResponse<>(HttpStatus.OK);
+    response.setData(foundBio);
+    response.setMessage("User's bio retrieved successfully");
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  @PostMapping("/profile")
+  public ResponseEntity<ApiResponse<ProfilePic>> updateProfile(@Valid @RequestBody ProfilePicDTO profile, HttpServletRequest request){
+    ProfilePic newProfile = profileService.uploadProfile(modelMapper.map(profile, ProfilePic.class), request);
+    ApiResponse<ProfilePic> response = new ApiResponse<>(HttpStatus.CREATED);
+    response.setData(newProfile);
+    response.setMessage("A user has updated is profile picture");
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/profile/{pic}")
+  public ResponseEntity<ApiResponse<String>> deleteProfile(@PathVariable(name = "pic") String profile, HttpServletRequest request) throws Exception {
+    profileService.delete(profile, request);
+    ApiResponse<String> response = new ApiResponse<>(HttpStatus.OK);
+    response.setMessage("Profile picture has been deleted");
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
   @PostMapping("/search")
-  public ResponseEntity<ApiResponse<User>> signup(@Valid @RequestBody SearchUsernameDto username, HttpServletRequest request){
+  public ResponseEntity<ApiResponse<User>> search(@Valid @RequestBody SearchUsernameDto username, HttpServletRequest request){
     User searchUser = userService.searchByUsername(username, request);
     ApiResponse<User> response = new ApiResponse<>(HttpStatus.OK);
     response.setData(searchUser);
