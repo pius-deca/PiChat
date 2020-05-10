858db1c830cd9b7017e342101aa93447f748f13e
PiusAremieye <ebi44ril@gmail.com>
2020-05-10 20:03:11 +0100

    refactored web security configuration

--- src/main/java/com/github/pius/pichats/security/WebSecurityConfig.java
+++ src/main/java/com/github/pius/pichats/security/WebSecurityConfig.java
@@ -20,19 +20,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private JwtProvider jwtProvider;
 
+
   @Override
-  public void configure(WebSecurity auth) throws Exception {
-    super.configure(auth);
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/auth/**")
+      .antMatchers("/swagger-config.yaml");
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    // Disable CSRF (cross site request forgery)
+    // enabled by default
     http.csrf().disable();
+
+    // No session will be created or used by spring security
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    http.authorizeRequests()
-      .antMatchers("/auth/**").permitAll()
-      .anyRequest().authenticated();
-    http.exceptionHandling().accessDeniedPage("/auth/login");
+
+    // set authentication policies for each routes
+    http.authorizeRequests().anyRequest().authenticated();
+
     http.apply(new JwtFilterConfigurer(jwtProvider));
   }
 
