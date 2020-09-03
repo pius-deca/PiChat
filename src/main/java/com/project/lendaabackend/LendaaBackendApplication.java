6652b5de6a040ac55b894ffe6e51926c59255c9a
PiusAremieye <ebi44ril@gmail.com>
2020-09-03 23:12:29 +0100

    initial commit

---
+++ src/main/java/com/project/lendaabackend/LendaaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.lendaabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendaaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendaaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/lendaabackend/LendaaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lendaabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendaaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
